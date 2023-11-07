bf1dbb6fa0c6df1d0964c56cf6434f7c9009c931
Xiang Li <lixiang020317@gmail.com>
2023-11-07 14:06:03 +0000

    node-lab1,  simple server

--- node-lab1/index.js
+++ node-lab1/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT;
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
